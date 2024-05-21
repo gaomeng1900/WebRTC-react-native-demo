a24b383a0016670b9d7078f7fc89fba2f514a41a
西萌 <gaomeng.gm@alibaba-inc.com>
2024-05-21 19:54:27 +0800

    feat: 尝试降低分辨率

--- rtc-headless.ts
+++ rtc-headless.ts
@@ -7,6 +7,7 @@ import {
   //   RTCView,
   //   MediaStreamTrack,
   mediaDevices,
+  RTCRtpSender,
   //   registerGlobals,
 } from 'react-native-webrtc';
 // registerGlobals();
@@ -70,6 +71,21 @@ export async function task() {
   const stream: MediaStream = await mediaDevices.getDisplayMedia(); // 这个库不支持参数
   console.log(chalk.greenBright('Got MediaStream:'), stream);
 
+  // @note: Not implemented.
+  // const track = stream.getVideoTracks()[0];
+  // const constraints = {
+  //   height: 720,
+  //   resizeMode: 'crop-and-scale',
+  // };
+  // try {
+  //   await track.applyConstraints(constraints);
+  // } catch (error) {
+  //   console.error(chalk.red('Error applying constraints:'), error);
+  // }
+
+  // const constraints = stream.getVideoTracks()[0].getConstraints();
+  // console.log(chalk.greenBright('MediaStream constraints:'), constraints);
+
   // rtc
 
   const peerConnection = new RTCPeerConnection(peerConnectionOptions);
@@ -100,6 +116,14 @@ export async function task() {
     peerConnection.addTrack(track, stream);
   });
 
+  const senders = peerConnection.getSenders();
+  // console.log(chalk.green('senders:'), senders);
+  // const params = senders[0].getParameters();
+  // console.log(chalk.green('params:'), params);
+  // console.log(chalk.green('encoding:'), params.encodings[0]);
+
+  await setVideoParams(senders[0]);
+
   (peerConnection as any).addEventListener('icecandidate', (event: any) => {
     // onicecandidate = event => {
     if (event.candidate) {
@@ -116,6 +140,35 @@ export async function task() {
       );
     }
   });
+
+  (peerConnection as any).addEventListener(
+    'connectionstatechange',
+    (event: any) => {
+      console.log(
+        chalk.green('Connection state changed:'),
+        event.target.connectionState,
+      );
+
+      if (event.target.connectionState === 'connected') {
+        console.log(chalk.green('Connected to peer'));
+
+        setTimeout(async () => {
+          const senders = peerConnection.getSenders();
+          console.log(chalk.green('senders:'), senders);
+          const params = senders[0].getParameters();
+          console.log(chalk.green('params:'), params);
+          console.log(chalk.green('encoding:'), params.encodings[0]);
+          // const capabilities = RTCRtpSender.getCapabilities('video');
+          // console.log(chalk.green('capabilities:'), capabilities);
+
+          setVideoParams(senders[0]).then(() => {
+            const newParams = senders[0].getParameters();
+            console.log(chalk.green('new encoding:'), newParams.encodings[0]);
+          });
+        }, 5000);
+      }
+    },
+  );
 
   peerConnection.createOffer({}).then(offer => {
     peerConnection.setLocalDescription(offer);
@@ -151,3 +204,39 @@ export async function task() {
 //     console.log(chalk.greenBright.bold.bgBlack('headless task running'));
 //   }, 1000);
 // }
+
+async function setVideoParams(sender: RTCRtpSender) {
+  // const track = sender.track;
+  // if (!track) {
+  //   console.error(chalk.red('No track found'));
+  //   return;
+  // }
+
+  // const height = 720;
+  // const scaleRatio = sender.track.getSettings().height! / height;
+  const params = sender.getParameters();
+
+  // If encodings is null, create it
+
+  // if (!params.encodings) {
+  //   params.encodings = [{}];
+  // }
+
+  // params.encodings[0].scaleResolutionDownBy = Math.max(scaleRatio, 1);
+  // @note: 并不总是生效
+  params.encodings[0].scaleResolutionDownBy = 2;
+  // @note: 降低码率后流畅性并没有提升
+  // params.encodings[0].maxBitrate = 15000;
+  params.encodings[0].maxFramerate = 20;
+  // @note: 这个参数会导致 app 闪退
+  // params.degradationPreference = 'maintain-framerate';
+  await sender.setParameters(params);
+
+  // If the newly changed value of scaleResolutionDownBy is 1,
+  // use applyConstraints() to be sure the height is constrained,
+  // since scaleResolutionDownBy may not be implemented
+
+  // if (sender.getParameters().encodings[0].scaleResolutionDownBy === 1) {
+  //   await sender.track.applyConstraints({height});
+  // }
+}
