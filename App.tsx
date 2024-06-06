6590b69324774216929a219cfac3fb2ce288fe1f
西萌 <gaomeng.gm@alibaba-inc.com>
2024-06-06 20:35:31 +0800

    feat: ui 更新

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -7,73 +6,19 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 
-import {RTC} from './RTC';
 import {Notice} from './Notice';
 // import {multiply, shell} from 'awesome-module';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-console.log('Hello World!');
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  // console.log('hahahhahah', shell('whoami'));
-
-  // shell(`input tap 757 757`);
-  // shell(`input swipe 100 500 100 300 10`);
-
-  // setTimeout(() => shell('sendevent /dev/input/event1 1 100 500'), 100);
-  // setTimeout(() => shell('sendevent /dev/input/event1 2 100 400'), 200);
-  // setTimeout(() => shell('sendevent /dev/input/event1 2 100 300'), 300);
-  // setTimeout(() => shell('sendevent /dev/input/event1 2 100 200'), 400);
-  // setTimeout(() => shell('sendevent /dev/input/event1 3 100 200'), 500);
-  // setTimeout(() => shell('sendevent /dev/input/event1 0 0 0'), 600);
-
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -96,22 +41,7 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-
-          {/*  */}
+          <Text>👋</Text>
         </View>
       </ScrollView>
       {/* <RTC /> */}
@@ -119,24 +49,5 @@ function App(): React.JSX.Element {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
